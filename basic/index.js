877de25841c84e3161353b2e233f338018cbd40d
Pepito94510 <kevin.boissiere94@gmail.com>
2024-02-15 15:22:19 +0100

    add calculator

--- basic/index.js
+++ basic/index.js
@@ -6,5 +6,27 @@ function goodbye() {
     return 'goodbye';
 }
 
+export function realCalculator(valueA, valueB, operator) {
+    switch (operator) {
+        case '+':
+            result = valueA + valueB;
+            break;
+        case '-':
+            result = valueA - valueB;
+            break;
+        case '*':
+            result = valueA * valueB;
+            break;
+        case '/':
+            result = valueA / valueB;
+            break;
+        default:
+            result = 'Error, this operator is unknow';
+            break;
+    }
+    return result;
+}
+
+
 export default { helloWorld, goodbye };
 
